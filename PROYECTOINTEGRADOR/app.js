05d5bea2ca0a23a8e29896d39bea22edbd9d9fd6
miaschwarzmaier <mschwarzmaier@udesa.edu.ar>
2024-04-07 12:54:22 -0300

    controladores y rutas

--- PROYECTOINTEGRADOR/app.js
+++ PROYECTOINTEGRADOR/app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productRouter = require('./routes/product');
+var registerRouter = require('./routes/register');
+var loginRouter = require('./routes/login');
+var editarperfilRouter = require('./routes/editarperfil');
 
 var app = express();
 
@@ -21,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//COMPLETAR!!!!!
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
