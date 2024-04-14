012add1a1e84f7ffc0be81c58c828cf5e55116f4
Victoria Valencia <mvalencia@udesa.edu.ar>
2024-04-14 12:06:07 -0300

    termine de arreglar las rutas y controladores para que no se rompan cuando se usan

--- PROYECTOINTEGRADOR/app.js
+++ PROYECTOINTEGRADOR/app.js
@@ -7,9 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
-var registerRouter = require('./routes/register');
-var loginRouter = require('./routes/login');
-var editarperfilRouter = require('./routes/editarperfil');
 
 var app = express();
 
@@ -25,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/product', productRouter);
 //COMPLETAR!!!!!
 
 
