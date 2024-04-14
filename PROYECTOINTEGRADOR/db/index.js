ea1cf2b248ccb1072d7236061470c350faa208b6
prog2-grupo2-RosellaValenciaSchwarzmaier <rosellac@udesa.edu.ar>
2024-04-14 13:03:56 -0300

    trabaja en el db y views

--- PROYECTOINTEGRADOR/db/index.js
+++ PROYECTOINTEGRADOR/db/index.js
@@ -2,16 +2,62 @@ const moduloDatos = {
     productos: [
         {
             id: 1,
-            idUsuarios: 1,
+            nombre: "Cafetera Moulineex",
+            descripcion: "Este es nuestro primer post",
+            imagen: "./images/products/img-cafetera-moulinex.jpg",
+            idUsuario: 1,
             comentarios: [
                 {
                     id: 1,
-                    idUsuarios: 1,
+                    idUsuario: 1,
                     texto: "Mi primer comentario"
 
                 }
             ]
-        }
+        },
+        {
+            id: 2,
+            nombre: "Cafetera Moulineex",
+            descripcion: "Este es nuestro segundo post",
+            idUsuario: 1,
+            comentarios: [
+                {
+                    id: 1,
+                    idUsuario: 1,
+                    texto: "Mi primer comentario"
+
+                }
+            ]
+        },
+        {
+            id: 3,
+            nombre: "Cafetera Moulineex",
+            descripcion: "Este es nuestro tercer post",
+            idUsuario: 1,
+            comentarios: [
+                {
+                    id: 1,
+                    idUsuario: 1,
+                    texto: "Mi primer comentario"
+
+                }
+            ]
+        },
+        {
+            id: 4,
+            nombre: "Cafetera Moulineex",
+            descripcion: "Este es nuestro cuarto post",
+            idUsuario: 1,
+            comentarios: [
+                {
+                    id: 1,
+                    idUsuario: 1,
+                    texto: "Mi primer comentario"
+
+                }
+            ]
+        },
+        
     ],
     usuarios: [
         {
