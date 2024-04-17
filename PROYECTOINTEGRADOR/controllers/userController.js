575ad2977de6e7ab3aec8d1b690348d2cf114dd5
miaschwarzmaier <mschwarzmaier@udesa.edu.ar>
2024-04-17 09:34:05 -0300

    user controller, perfiles

--- PROYECTOINTEGRADOR/controllers/userController.js
+++ PROYECTOINTEGRADOR/controllers/userController.js
@@ -4,13 +4,31 @@ const userController = {
     login: function(req, res) {
         return res.render('login');
     },
-    registro: function(req, res) {
+    register: function(req, res) {
         return res.render('register');
     },
-    miPerfil: function(req, res) {
+    profile: function(req, res) { // EL PERFIL DE OTRO USUARIO
         let id = req.params.id;
         let usuario = {};
+    
+        for (let i = 0; i < moduloDatos.usuarios.length; i++){
+            if (moduloDatos.usuarios[i].id == id){
+                usuario = moduloDatos.usuarios[i]
+            }
+        }
+        let publicaciones = [];
+        for (let i = 0; i < moduloDatos.productos.length; i++){
+            if (moduloDatos.productos[i].idUsuario == id){
+                publicaciones.push(moduloDatos.productos[i])
+            }
+        }
 
+        return res.render('profile', { usuario: usuario, publicaciones: publicaciones, miusuario: false, mispublicaciones: false });
+    },
+    myProfile: function(req, res) { // MI PERFIL
+        let id = req.params.id;
+        let usuario = {};
+    
         for (let i = 0; i < moduloDatos.usuarios.length; i++){
             if (moduloDatos.usuarios[i].id == id){
                 usuario = moduloDatos.usuarios[i]
@@ -23,11 +41,10 @@ const userController = {
             }
         }
     
-
-        return res.render('miPerfil', { usuario: usuario, publicaciones: publicaciones });
+        return res.render('profile', { miusuario: usuario, mispublicaciones: publicaciones, usuario: false, publicaciones: false });
     },
-    editarMiPerfil: function(req, res) {
-        return res.render('editarMiPerfil');
+    editProfile: function(req, res) {
+        return res.render('profile-edit');
     },
 };
 
