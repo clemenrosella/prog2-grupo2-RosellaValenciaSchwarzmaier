59961a3990a01d4b7c68365370e8d275ca26b946
miaschwarzmaier <mschwarzmaier@udesa.edu.ar>
2024-05-13 18:04:47 -0300

    database product y comment

--- PROYECTOINTEGRADOR/database/models/Comment.js
+++ PROYECTOINTEGRADOR/database/models/Comment.js
@@ -1,15 +1,42 @@
 module.exports = function(sequelize, dataTypes){
 
-    let alias = ""
+    let alias = "Comment"
 
     let cols ={
+        id:{
+            autoIncrement: true,
+            primarKey: true,
+            type: dataTypes.INTEGER,
+        },
 
+        texto_comentario:{
+            type: dataTypes.STRING(500)
+        },
+        createdAt:{
+            type: dataTypes.DATE
+        },
 
+        updatedAt:{
+            type: dataTypes.DATE
+        },
 
+        deletedAt:{
+            type: dataTypes.DATE
+        },
+
+        id_usuario:{
+            type: dataTypes.INTEGER
+        },
+
+        id_producto:{
+            type: dataTypes.INTEGER
+        }
     }
 
     let config= {
-
+        tableName: "comentario",
+        timestamps:true,
+        underscored: true,
     }
 
     let Comment = sequelize.define(alias, cols, config)
--- PROYECTOINTEGRADOR/database/models/Product.js
+++ PROYECTOINTEGRADOR/database/models/Product.js
@@ -1,15 +1,44 @@
 module.exports = function(sequelize, dataTypes){
 
-    let alias = ""
+    let alias = "Product"
 
     let cols ={
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER, 
+         },
 
+        imagen:{
+            type: dataTypes.STRING
+        },
+        
+        nombre:{
+            type: dataTypes.STRING
+        },
 
+        descripcion:{
+            type: dataTypes.STRING
+        },
+
+        createdAt:{
+            type: dataTypes.DATE
+        },
+
+        updatedAt:{
+            type: dataTypes.DATE
+        },
+
+        deletedAt:{
+            type: dataTypes.DATE
+        },
 
     }
 
     let config= {
-
+        tableName: "productos",
+        timestamps: true,
+        underscored: true,
     }
 
     let Product = sequelize.define(alias, cols, config)
