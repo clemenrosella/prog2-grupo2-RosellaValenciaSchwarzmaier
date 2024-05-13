bc778029443d4dbd967c40673e3b4f13e76074b0
Victoria Valencia <mvalencia@udesa.edu.ar>
2024-05-13 17:36:01 -0300

    modifique el modelo de User

--- PROYECTOINTEGRADOR/database/models/User.js
+++ PROYECTOINTEGRADOR/database/models/User.js
@@ -1,15 +1,43 @@
 module.exports = function(sequelize, dataTypes){
 
-    let alias = ""
+    let alias = "User"
 
     let cols ={
-
-
-
+        id: {
+            autoIncrenent: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        email: {
+            type: dataTypes.STRING(500)
+        },
+        contraseña: {
+            type: dataTypes.STRING(500)
+        },
+        fecha: {
+            type: dataTypes.DATE
+        },
+        dni: {
+            type:dataTypes.INTEGER
+        },
+        foto_de_perfil:{
+            type: dataTypes.STRING
+        },
+        createdAt: {
+            type: dataTypes.DATE
+        },
+        updatedAt:{
+            type: dataTypes.DATE
+        },
+        deleatedAt: {
+            type: dataTypes.DATE
+        },
     }
 
     let config= {
-
+        tableName: "usuarios",
+        timestamps: true,
+        underscored: true,
     }
 
     let User = sequelize.define(alias, cols, config)
